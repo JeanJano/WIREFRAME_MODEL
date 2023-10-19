cbc7c5a627005a8ef513136ad60ec7710eef21d5
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2023-10-19 15:53:22 +0200

    choose file working but the map work only if it is a small one and crash when keypress

--- fdf.js
+++ fdf.js
@@ -1,56 +1,113 @@
-document.getElementById("fileInput").addEventListener("change", function (event) {
-	const file = event.target.files[0];
-	const reader = new FileReader();
-	reader.onload = function (event) {
-		const contents = event.target.result;
-		const lines = contents.split("\n");
-		const output = document.getElementById("output");
-		for (const line of lines) {
-			console.log(line);
-			output.textContent += line + "\n";
-		}
-	};
-	reader.readAsText(file);
-});
-
 const canvas = document.getElementById('pixelCanvas');
 
 const ctx = canvas.getContext('2d');
 
-ctx.strokeStyle = 'blue';
+ctx.strokeStyle = 'red';
 ctx.lineWidth = 1;
 
 const height = 10;
 const width = 10;
 let teta = 0;
 let press = 0;
-let x_axis = 10;
-let y_axis = 10;
+let x_axis = 0;
+let y_axis = 0;
 let zoom = 20;
 
 class Map {
 	point = [];
 	color = [];
-	constructor() {
+	constructor(callback) {
+		document.getElementById("fileInput").addEventListener("change", (event) => {
+			const file = event.target.files[0];
+			const reader = new FileReader();
+			reader.onload = (event) => {
+			  const contents = event.target.result;
+			  const lines = contents.split("\n");
+			  const output = document.getElementById("output");
+			  
+			  for (const line of lines) {
+				// console.log(line);
+				output.textContent += line + "\n";
+				let split_line = line.split(" ");
+				
+				this.point[x_axis] = this.point[x_axis] || [];
+				this.color[x_axis] = this.color[x_axis] || [];
+				
+				for (let y = 0; y < split_line.length; y++) {
+				  let split_val = split_line[y].split(",");
+				//   console.log(split_val[0]);
+				  this.point[x_axis][y] = split_val[0];
+				  if (split_val.length === 1) {
+					this.color[x_axis][y] = '0x0000FF';
+				  } else {
+					this.color[x_axis][y] = split_val[1];
+				  }
+				}
+				
+				if (x_axis === 0) {
+				  y_axis = split_line.length;
+				}
+				
+				x_axis++;
+			  }
+			  
+			  // Trim the arrays to the actual size
+			  this.point.length = x_axis;
+			  this.color.length = x_axis;
+			  this.point.forEach(row => {
+				row.length = y_axis;
+			  });
+			  this.color.forEach(row => {
+				row.length = y_axis;
+			  });
+			  callback();
+			};
+			
+			reader.readAsText(file);
+		  });
 	};
+
+	getPoint() {
+		return (this.point);
+	}
+
+	getColor() {
+		return (this.color);
+	}
+
+	log() {
+		console.log(x_axis);
+		for (let i = 0; i < x_axis; i++) {
+			for (let j = 0; j < y_axis; j++) {
+				console.log(this.point[i][j] + " " + this.color[i][j]);
+			}
+		}
+	}
 }
 
-let array = [
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-	[0, 1, 2, 2, 2, 2, 2, 2, 1, 0],
-	[0, 1, 2, 3, 3, 3, 3, 2, 1, 0],
-	[0, 1, 2, 3, 0, 0, 3, 2, 1, 0],
-	[0, 1, 2, 3, 0, 0, 3, 2, 1, 0],
-	[0, 1, 2, 3, 3, 3, 3, 2, 1, 0],
-	[0, 1, 2, 2, 2, 2, 2, 2, 1, 0],
-	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
+// let array = [
+// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+// 	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+// 	[0, 1, 2, 2, 2, 2, 2, 2, 1, 0],
+// 	[0, 1, 2, 3, 3, 3, 3, 2, 1, 0],
+// 	[0, 1, 2, 3, 0, 0, 3, 2, 1, 0],
+// 	[0, 1, 2, 3, 0, 0, 3, 2, 1, 0],
+// 	[0, 1, 2, 3, 3, 3, 3, 2, 1, 0],
+// 	[0, 1, 2, 2, 2, 2, 2, 2, 1, 0],
+// 	[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+// ];
 
 function drawBackground() {
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawLine(arr1, arr2) {
+	ctx.beginPath();
+	ctx.moveTo(arr1[0], arr1[1]);
+	ctx.lineTo(arr2[0], arr2[1]);
+	ctx.stroke();
 }
 
 // import {rotateX, rotateY, rotateZ} from './test.js';
@@ -138,9 +195,10 @@ function rotation(x, y, z) {
 		return ([x, y, z]);
 }
 
-function calcIso(x, y, array) {
+function calcIso(x, y, map) {
 	let res = [];
-	let vector = rotation(x, y, array[x][y]);
+	let point = map.getPoint();
+	let vector = rotation(x, y, point[x][y]);
 
 	res[0] = (Math.sqrt(2)/2) * (vector[0] - vector[1]);
 	res[1] = (-Math.sqrt(2)/3) * vector[2] - ((1/Math.sqrt(6)) * -(vector[0] + vector[1]));
@@ -150,28 +208,24 @@ function calcIso(x, y, array) {
 	return (res);
 }
 
-function drawLine(arr1, arr2) {
-	ctx.beginPath();
-	ctx.moveTo(arr1[0], arr1[1]);
-	ctx.lineTo(arr2[0], arr2[1]);
-	ctx.stroke();
-}
-
-function drawAll() {
+function drawAll(map) {
 	let transform1;
 	let transform2;
-	for (let i = 0; i < height; i++) {
-		for (let j = 0; j < width; j++) {
-			transform1 = calcIso(i, j, array);
-			if (i < height - 1) {
-				transform2 = calcIso(i + 1, j, array);
+	for (let i = 0; i < x_axis; i++) {
+		for (let j = 0; j < y_axis; j++) {
+			transform1 = calcIso(i, j, map);
+			if (i < x_axis - 1) {
+				transform2 = calcIso(i + 1, j, map);
 				drawLine(transform1, transform2);
 			}
-			transform2 = calcIso(i, j + 1, array);
+			transform2 = calcIso(i, j + 1, map);
 			drawLine(transform1, transform2);
 		}
 	}
 }
 
-drawBackground();
-drawAll();
+const map = new Map(() => {
+	// map.log();
+	drawBackground();
+	drawAll(map);
+});
